ea60fd7778052b4ba6da806371c26b81a2807800
UnknownStrange <princenyarkoedwin@gmail.com>
2026-03-08 18:04:55 +0000

    done with CRUD functions

--- orm_lab/management/urls.py
+++ orm_lab/management/urls.py
@@ -1,7 +1,11 @@
+from os import name
 from django.urls import path 
 from .import views
 
 urlpatterns = [
     path('add/', views.add_student, name='add_student'),
     path('list/',views.list_students, name='list_students'),
+    path('<int:id>/', views.student_detail, name='student_detail'),
+    path('edit/<int:id>/', views.edit_student, name='edit_student'),
+    path('delete/<init:id', views.delete_student, name='delete_student'),
 ]
--- orm_lab/management/views.py
+++ orm_lab/management/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect 
+from django.shortcuts import render,redirect, get_object_or_404
 from core.models import Student, Course
 from .forms import StudentForms
 
@@ -14,5 +14,41 @@ def add_student(request):
     return render(request, 'management/add_student.html', {'form': form})
 
 def list_students(request):
-    students = Student.objects.select_related().prefetch_related('courses').all()
-    return render(request, 'management/list_students.html', {'students': students})
+   student = Student.object.all()
+   
+   name = request.GET.get('name')
+   course = request.GET.get('course')
+   
+   if name:
+       student = student.filter(name_icontains=name)
+       
+   if course:
+       student = student.filter(courses_tittle_icontains=course)
+   
+   return render(request, 'list_students.html', {'student': student})
+
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+    
+    return render(request, "student_detail.html", {'student':student})
+
+def edit_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    
+    if request.method == "POST":
+        form = StudentForms(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('list_students.html')
+    else:
+        form = StudentForms(instance=student)
+    
+    return render(request, 'edit_student.html', {'form': form})
+        
+def delete_student(request, id):
+    student = get_object_or_404(Student,id=id)
+    
+    if request.method == "POST":
+        student.delete()
+        return redirect('list_students')
+    return render(request, "delete_student.html", {"student": student})
